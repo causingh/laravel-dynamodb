d505e456235eec1261b616a4e4e186bacb779417
Harold Glenn Causing <causinghglenn@gmail.com>
2021-09-07 00:08:29 +0800

    prevent casting date attributes

--- src/DynamoDbModel.php
+++ src/DynamoDbModel.php
@@ -395,6 +395,41 @@ abstract class DynamoDbModel extends Model
     }
 
     /**
+     * Transform a raw model value using mutators, casts, etc.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return mixed
+     */
+    protected function transformModelValue($key, $value)
+    {
+        // If the attribute has a get mutator, we will call that then return what
+        // it returns as the value, which is useful for transforming values on
+        // retrieval from the model to a form that is more useful for usage.
+        if ($this->hasGetMutator($key)) {
+            return $this->mutateAttribute($key, $value);
+        }
+
+        // If the attribute exists within the cast array, we will convert it to
+        // an appropriate native PHP type dependent upon the associated value
+        // given with the key in the pair. Dayle made this comment line up.
+        if ($this->hasCast($key)) {
+            return $this->castAttribute($key, $value);
+        }
+
+        // Date attribute casting removed as DynamoDB do not support Date attribute.
+        // If the attribute is listed as a date, we will convert it to a DateTime
+        // instance on retrieval, which makes it quite convenient to work with
+        // date fields without having to create a mutator for each property.
+        // if ($value !== null
+        //     && \in_array($key, $this->getDates(), false)) {
+        //     return $this->asDateTime($value);
+        // }
+
+        return $value;
+    }
+
+    /**
      * Get the primary key for the model.
      *
      * @return string
